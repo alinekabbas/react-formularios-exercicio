e450d0578fc4b97fd6fd7f6af9a48d4ac43136ea
alinekabbas <alinekabbas@gmail.com>
2022-11-24 12:49:08 -0300

    exercicio concluido

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,31 +1,58 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 import { MainContainer, Form, Input } from './styles'
 
 function MainPage() {
+  const [form, onChangeForm] = useForm({nome:"", modulos: "", tecnologias:"", responsavel:""})
+
+  const handleClick = (event)=>{
+    event.preventDefault()
+    console.log(form)
+  }
  
   return (
     <MainContainer>
       <h2>Cadastro de novo curso</h2>
 
-      <Form onSubmit="">
+      <Form onSubmit={handleClick}>
         <label for="nome">Nome do Curso:</label>
         <Input 
           id="nome"
+          name='nome'
+          value={form.nome}
+          onChange={onChangeForm}
+          required
         />
 
         <label for="modulos">Número de Módulos: </label>
         <Input 
           id="modulos"
+          name='modulos'
+          value={form.modulos}
+          onChange={onChangeForm}
+          pattern='[2-9]'
+          title='O número de módulos não pode ser menor que 2'
+          required
         />
 
         <label for="tecnologias">Tecnologias abordadas: </label>
         <Input 
           id="tecnologias"
+          name='tecnologias'
+          value={form.tecnologias}
+          onChange={onChangeForm}
+          required
         />
         
         <label for="responsavel">Responsável: </label>
         <Input 
           id="responsavel"
+          name='responsavel'
+          value={form.responsavel}
+          onChange={onChangeForm}
+          pattern='[A-Za-z0-9]{5,}'
+          title='O nome do responsável deve ter ao menos 5 caracteres (letras e/ou números)'
+          required
         />
       <button type="submit">Criar curso</button>
       </Form>
